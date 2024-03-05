85068c1d1826406f5ac36d3d0de9212582065363
shabbirbd <shabbirhossainshah@gmail.com>
2024-03-05 19:29:01 +0600

    mobileNav done

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -85,7 +85,7 @@ const Sidebar = () => {
 
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href={"sign-in"}>Login</Link>
+              <Link href={"/sign-in"}>Login</Link>
             </Button>
           </SignedOut>
         </nav>

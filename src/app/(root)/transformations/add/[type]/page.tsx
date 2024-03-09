0b474d7083f7a766c12112b1c2a269a696026b83
shabbirbd <shabbirhossainshah@gmail.com>
2024-03-09 19:16:57 +0600

    adding component

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -4,9 +4,11 @@ import { transformationTypes } from "@/constants";
 const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
   const transformation = transformationTypes[type];
   return (
-    <div>
+    <>
       <Header title={transformation.title} subtitle={transformation.subTitle} />
-    </div>
+
+      {/* <TransformationForm action="Add" /> */}
+    </>
   );
 };
 
